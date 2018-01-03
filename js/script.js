e8245938523ca38cc8de75e154b44932cb836f28
Karolina Siwkowska <trallala00@gmail.com>
2018-01-03 18:40:23 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function drawTree(h) {
+    for (var i = 0; i <= h; i++) {
+        var star = "";
+        for (var j = 1; j <= 1 * i; j++) {
+            star += "*";
+        }
+        console.log("%c%s", "font-size: 20px; color: green",
+            star);
+    }
+}
+drawTree(1)
+
+function drawTree(h) {
+    for (var i = 0; i <= h; i++) {
+        var star = "";
+        for (var j = 1; j <= 1 * i; j++) {
+            star += "*";
+        }
+        console.log("%c%s", "font-size: 20px; color: green",
+            star);
+    }
+}
+drawTree(2)
+
+function drawTree(h) {
+    for (var i = 0; i <= h; i++) {
+        var star = "";
+        for (var j = 1; j <= 1 * i; j++) {
+            star += "*";
+        }
+        console.log("%c%s", "font-size: 20px; color: green",
+            star);
+    }
+}
+drawTree(5)
